195d1fd638e6a30a34c685033d085ec6a45d1528
blockiosaurus <blockiosaurus@gmail.com>
2023-12-19 06:05:55 -0500

    Updating kinobi config.

--- configs/kinobi.cjs
+++ configs/kinobi.cjs
@@ -16,36 +16,36 @@ kinobi.update(
 );
 
 // Update accounts.
-kinobi.update(
-  new k.UpdateAccountsVisitor({
-    myPdaAccount: {
-      seeds: [
-        k.stringConstantSeed("myPdaAccount"),
-        k.programSeed(),
-        k.publicKeySeed("authority", "The address of the authority"),
-        k.stringSeed("name", "The name of the account"),
-      ],
-    },
-  })
-);
+// kinobi.update(
+//   new k.UpdateAccountsVisitor({
+//     myPdaAccount: {
+//       seeds: [
+//         k.stringConstantSeed("myPdaAccount"),
+//         k.programSeed(),
+//         k.publicKeySeed("authority", "The address of the authority"),
+//         k.stringSeed("name", "The name of the account"),
+//       ],
+//     },
+//   })
+// );
 
 // Update instructions.
-kinobi.update(
-  new k.UpdateInstructionsVisitor({
-    create: {
-      bytesCreatedOnChain: k.bytesFromAccount("myAccount"),
-    },
-  })
-);
+// kinobi.update(
+//   new k.UpdateInstructionsVisitor({
+//     create: {
+//       bytesCreatedOnChain: k.bytesFromAccount("myAccount"),
+//     },
+//   })
+// );
 
 // Set ShankAccount discriminator.
-const key = (name) => ({ field: "key", value: k.vEnum("Key", name) });
-kinobi.update(
-  new k.SetAccountDiscriminatorFromFieldVisitor({
-    myAccount: key("MyAccount"),
-    myPdaAccount: key("MyPdaAccount"),
-  })
-);
+// const key = (name) => ({ field: "key", value: k.vEnum("Key", name) });
+// kinobi.update(
+//   new k.SetAccountDiscriminatorFromFieldVisitor({
+//     myAccount: key("MyAccount"),
+//     myPdaAccount: key("MyPdaAccount"),
+//   })
+// );
 
 // Render JavaScript.
 const jsDir = path.join(clientDir, "js", "src", "generated");
